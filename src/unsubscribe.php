666b082ba57af26860ed8a9b9c6eb813c7b3b7ca
ayushikarajput1810 <ayushikarajput1810@gmail.com>
2025-06-15 16:24:39 +0530

    Update unsubscribe.php

--- src/unsubscribe.php
+++ src/unsubscribe.php
@@ -1,1 +1,12 @@
+<?php
+require_once 'functions.php';
+
+if (isset($_GET['email'])) {
+    $email = urldecode($_GET['email']);
+    unsubscribeEmail($email);
+
+    echo "<h2>You have been unsubscribed successfully. 🙌</h2>";
+} else {
+    echo "<h2>Invalid unsubscribe request.</h2>";
+}
 
